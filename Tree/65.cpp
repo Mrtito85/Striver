0396bbae98cfa1a48b907e4b97928ff0c256c89f
Mrtito85 <114670888+Mrtito85@users.noreply.github.com>
2025-06-05 04:54:36 +0500

    k path sum

--- Tree/65.cpp
+++ Tree/65.cpp
@@ -66,7 +66,7 @@ void solve(node* &root,int sum,int &maxSum,int len,int &maxLength){
 
         sum+=root->data;
         solve(root->left,sum,maxSum,len+1,maxLength);
-        solve(root->right,sum,maxSum,len,maxLength);
+        solve(root->right,sum,maxSum,len+1,maxLength);
 }
 void findLargestPathSum(node* &root){
     int sum=0;
@@ -76,10 +76,54 @@ void findLargestPathSum(node* &root){
     solve(root,sum,maxSum,len,maxLength);
     cout<<"Largest path sum is "<<maxSum<<endl;
 }
+
+node* lowestCommonInsister(node* root,int n1,int n2){
+    if(root==NULL){
+        return NULL;
+    }
+    if(root->data==n1 || root->data==n2){
+        return  root;
+    }
+    node* left=lowestCommonInsister(root->left,n1,n2);
+    node* right=lowestCommonInsister(root->right,n1,n2);
+
+    if(left!=NULL && right!=NULL){
+        return root;
+    }
+    else if(left!=NULL && right==NULL){
+        return left;
+    }else if(left==NULL && right!=NULL){
+        return right;
+    }
+    else{
+        return NULL;
+    }
+}
+void solve1(node* &root,int k,int &count,vector<int> path){
+    if(root==NULL)return;
+    path.push_back(root->data);
+    solve1(root->left,k,count,path);
+    solve1(root->right,k,count,path);
+    int sum=0;
+    int n=path.size();
+    //here we traverse from leaf
+    for(int i=n-1;i>=0;i--){
+        sum+=path[i];
+        if(sum==k)count++;
+    }
+    path.pop_back();
+}
+int kSumPath(node* &root,int k){
+    int count=0;
+    vector<int> path;
+    solve1(root,k,count,path);
+    return count;
+}
 int main(){
     node* root=NULL;
     root=buildTree(root);
     levelOrder(root);
     findLargestPathSum(root);
+    cout<<"lowest common insister "<<lowestCommonInsister(root,50,60)->data;// 40
     return 0;
 }
