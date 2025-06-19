609d34c37d0aed9f98689bccd30fc8480ce889cb
Mrtito85 <114670888+Mrtito85@users.noreply.github.com>
2025-06-19 15:11:58 +0500

    fibonacci series

--- Greedy/Untitled-4.py
+++ Greedy/Untitled-4.py
@@ -1,7 +1,7 @@
->>> sentence-1 = "HEY"
->>> sentence-1.isupper
-True
->>> sen-2 = "hey"
->>> sen-2.isupper()
-False
->>> sen
+# >>> sentence-1 = "HEY"
+# >>> sentence-1.isupper
+# True
+# >>> sen-2 = "hey"
+# >>> sen-2.isupper()
+# False
+# >>> sen
