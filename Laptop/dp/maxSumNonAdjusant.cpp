cfaea76ca038323fa6b2bd3064c90570783ea64d
DESKTOP-IQFL2I9\k n <zahid555681@gmail.com>
2025-06-20 02:21:58 +0500

    space opptize non adj max sum

--- Laptop/dp/maxSumNonAdjusant.cpp
+++ Laptop/dp/maxSumNonAdjusant.cpp
@@ -24,6 +24,38 @@ int maxSumNonAdjRec(vector<int> &arr,int index,vector<int> &dp){
     int right=0 + maxSumNonAdjRec(arr,index-1,dp);
     return dp[index] =  max(left,right);
 }
+int maxSumNonAdjTab(vector<int> &arr,int index){
+    vector<int> dp(index+1,0);
+    dp[0]=arr[0];
+    int neg=0;
+    for(int i=1;i<=index;i++){
+        int take=arr[i];
+        if(i>1){
+            take+=dp[i-2];
+        }
+        int notTake= dp[i-1];
+        dp[i]=max(take,notTake);
+    }
+    return dp[index];
+}
+int maxSumNonAdjTabSpaceOptimize(vector<int> &arr,int index){
+    
+    int prev1=arr[0];
+    int prev2=0;
+    for(int i=1;i<=index;i++){
+        int take=arr[i];
+        if(i>1){
+            take+=prev2;
+        }
+        int notTake= prev1;
+        int curr=max(take,notTake);
+        prev2=prev1;
+        prev1=curr;
+    }
+    return prev1;
+}
+
+
 
 int main()
 {
@@ -35,6 +67,7 @@ int main()
     }
     //cout<<maxSumNonAdj(arr,n-1);
     vector<int> dp(n+1,-1);
-    cout<<maxSumNonAdjRec(arr,n-1,dp);
+    //cout<<maxSumNonAdjRec(arr,n-1,dp);
+    cout<<maxSumNonAdjTabSpaceOptimize(arr,n-1);
     return 0;
 }
